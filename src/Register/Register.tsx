6ec4aef09b65428dc357408dca891613c22ed3f8
RamakrishnaGithub <panchireddyp197@gmail.com>
2024-08-13 21:35:07 +0530

    first commit

---
+++ src/Register/Register.tsx
@@ -0,0 +1,44 @@
+"use client"
+import React, { useState } from 'react'
+
+const Register = () => {
+    const [data, setData] = useState({})
+    const fnChange = (eve: any) => {
+        const { name, value } = eve.target
+        setData({ ...data, [name]: value })
+    }
+    const fnSubmit = async () => {
+        try {
+            const dataobj = {
+                "data": data
+            }
+            const res = await fetch("http://localhost:2020/std/register", {
+                method: "post",
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                body: JSON.stringify(dataobj)
+            })
+            const result=await res.json()
+            console.log(result)
+        }catch(ex){
+            console.error(ex)
+        }
+        
+    }
+    return (
+        <div>
+            <h1>Registration</h1>
+            <p><b>Name:</b><input name="name" type="text" onChange={fnChange} />
+            </p>
+            <p><b>Roll NO:</b><input name="rno" type="number" onChange={fnChange} />
+            </p>
+            <p><b>Location:</b><textarea name="loc" onChange={fnChange} />
+            </p>
+            <p><button onClick={fnSubmit}>Submit</button>
+            </p>
+        </div>
+    )
+}
+
+export default Register
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Register from "@/Register/Register";
+
+export default function Home() {
+  return (<div>
+  <Register/>
+  </div>
+        
+  );
+}
